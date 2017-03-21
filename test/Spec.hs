513ba2e6397236bc9f1027f717ce990460e23798
Pavel Yakovlev <zhmactep@gmail.com>
2017-03-21 10:47:05 +0300

    Hackage release

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,8 +6,8 @@ import           Test.Hspec
 
 main :: IO ()
 main = hspec $
-  describe "Hello World" $
-    it "should work" $ do
+  describe "MMTF" $
+    it "should parse 1FSD" $ do
       contents <- B.readFile "resource/1FSD.mmtf"
       m <- decode contents
       let sid = (structureId . structure) m
